2ce32c1c9e96e2062d6d45c8be30849870765fa3
kkanayaaa <kanayaanantanisyafikri@gmail.com>
2026-09-21 17:22:06 +0700

    add endpoint API

--- hunika-backend/routes/api.php
+++ hunika-backend/routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Api\KostController;
+use App\Http\Controllers\BookingController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\ReviewController;
 
 Route::get('/test', function () {
     return response()->json([
@@ -9,6 +13,14 @@ Route::get('/test', function () {
         'message' => 'REST API Hunika berhasil!',
     ]);
 });
-
+//public routes 
+Route::post('/login', [AuthController::class, 'login']);
 Route::get('/kost', [KostController::class, 'index']);
 Route::get('/kost/{id}', [KostController::class, 'show']);
+
+//protected routes (need login)
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/bookings', [BookingController::class, 'store']);
+    Route::post('/payments/{booking_id}', [PaymentController::class, 'uploadProof']);
+    Route::post('/reviews', [ReviewController::class, 'store']);
+});
